1ef2e06820a9f8ed8a85fa616620f5fb5de3d9cd
Rachel <rachelsuk97@gmail.com>
2021-02-04 03:42:42 +0000

    gave users choices of seeing all ratings, adding restaurant or quitting program

--- ratings.py
+++ ratings.py
@@ -1,5 +1,5 @@
 """Restaurant rating lister."""
-
+import random
 def create_dict_ratings(file_name):
     file = open(file_name)
     rest_ratings = {}
@@ -30,11 +30,17 @@ def add_rest_rating(dictionary):
 
 restaurant_ratings = create_dict_ratings("scores.txt")
 while True:
-    command = int(input("What would you like to do? Enter 1 to see all the restaurant ratings, 2 to add a new restaurant rating and 3 to quit: "))
+    command = int(input("What would you like to do? Enter 1 to see all the restaurant ratings, 2 to add a new restaurant rating, 3 to update a random restaurant and 4 to quit: "))
     if command == 1:
         for restaurant, rating in sort_alpha(restaurant_ratings):
             print(f"{restaurant} is rated at {rating}.")
     elif command == 2:
         restaurant_ratings = add_rest_rating(restaurant_ratings)
     elif command == 3:
+        restaurant = random.choice(restaurant_ratings)
+        rating = int(input(f'what would you like to rate {restaurant}?'))
+        restaurant_ratings[restaurant] = rating
+    elif command == 4:
         break
+    else:
+        print("Not a valid command. Try again.")
