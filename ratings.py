849405a5de2df084dada9ba0a6d49b3fde269857
Rachel <rachelsuk97@gmail.com>
2021-02-04 04:16:35 +0000

    users can udpate a chosen restaurant rating

--- ratings.py
+++ ratings.py
@@ -12,11 +12,11 @@ def sort_alpha(dictionary):
     sorted_list = sorted(dictionary.items())
     return sorted_list 
 
-def add_rest_rating(dictionary):
+def add_rest_rating(restaurant, dictionary):
     while True:
         try: 
-            restaurant = input("What is the restaurant name? ").rstrip()
-            restaurant_score = int(input("What is the restaurant score? ").rstrip())
+
+            restaurant_score = int(input(f"What is {restaurant}'s score? ").rstrip())
 
             if 1 <= restaurant_score <= 5:
                 dictionary[restaurant] = restaurant_score
@@ -27,20 +27,52 @@ def add_rest_rating(dictionary):
         except ValueError:
             print ("Rating must be an integer! Try again.")
     return dictionary
+def update_rest_rating(dictionary):
+    while True:
+        try: 
+            restaurant = input("What is the restaurant name? ").rstrip()
+            if restaurant in dictionary:
+                update = input(f'{restaurant} has already been rated a {dictionary[restaurant]}. Would you like to update the rating? Y for yes and N for no. ')
+                if update.lower == 'y':
+                    restaurant_score = int(input(f"What is {restaurant}'s new score? ").rstrip())
+                elif update.lower == 'n':
+                    break
+                else:
+                    print("Not a valid entry. try again.")
+            elif restaurant not in dictionary:
+                restaurant_score = int(input(f"{restaurant} has not been rated yet. What is {restaurant}'s score? ").rstrip())
 
+            if 1 <= restaurant_score <= 5:
+                dictionary[restaurant] = restaurant_score
+                break 
+            else:
+                print("Rating must be 1-5. Try again.")
+            
+        except ValueError:
+            print ("Rating must be an integer! Try again.")
+    return dictionary
 restaurant_ratings = create_dict_ratings("scores.txt")
 while True:
-    command = int(input("What would you like to do? Enter 1 to see all the restaurant ratings, 2 to add a new restaurant rating, 3 to update a random restaurant and 4 to quit: "))
+    command = int(input("What would you like to do? Enter 1 to see all the restaurant ratings, 2 to add or update a new restaurant rating, and 3 to quit: "))
     if command == 1:
         for restaurant, rating in sort_alpha(restaurant_ratings):
             print(f"{restaurant} is rated at {rating}.")
     elif command == 2:
-        restaurant_ratings = add_rest_rating(restaurant_ratings)
+        while True:
+            restaurant = input("What is the restaurant's name? ").rstrip()
+            if restaurant in restaurant_ratings:
+                update = input(f'{restaurant} has already been rated a {restaurant_ratings[restaurant]}. Would you like to update the rating? Y for yes and N for no. ')
+                if update.lower() == 'y':
+                    restaurant_ratings = add_rest_rating(restaurant, restaurant_ratings)
+                    break
+                elif update.lower() == 'n':
+                    break
+                else:
+                    print("Not a valid command. Try again.")
+            elif restaurant not in restaurant_ratings:
+                restaurant_ratings = add_rest_rating(restaurant, restaurant_ratings)
+                break
     elif command == 3:
-        restaurant = random.choice(list(restaurant_ratings.keys()))
-        rating = int(input(f'what would you like to rate {restaurant}? '))
-        restaurant_ratings[restaurant] = rating
-    elif command == 4:
         break
     else:
         print("Not a valid command. Try again.")
